af87f574919f0daaaca4340efc7dd671f7651568
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-10-20 23:50:59 +0300

    router, popup, triangle

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames'
 
-function Button({onClick, className, outline, children}) {
+function Button({ className, outline, children}) {
     return (
         <button className={classNames('button', className, {
             'button-outline': outline
